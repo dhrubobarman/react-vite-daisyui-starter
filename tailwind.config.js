733ceb8f71117281fd356702a0bffbc49c72f15e
Dhrubojyoti Barman <47059504+dhrubobarman@users.noreply.github.com>
2024-09-24 20:38:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ import daisyui from 'daisyui';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+     container: {
+      center: true,
+      padding: '1rem'
+    },
     extend: {}
   },
   plugins: [daisyui]
